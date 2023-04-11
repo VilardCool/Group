5e7c11de75caecf1b5faaaf739f705c8ccadd153
VilardCool <61537368+VilardCool@users.noreply.github.com>
2023-04-11 09:44:22 +0300

    Update bot.py

--- SPP/Bot/bot.py
+++ SPP/Bot/bot.py
@@ -1,30 +1,40 @@
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters
+import requests
+from config import BOT_TOKEN
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
+def movie_handler(update, context):
+    id = int(update.message.from_user.id)
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
+    print(id)
+    first_name = update.message.chat.first_name
+    opening_line = f"""Hi {first_name}!"""
+    update.message.reply_text(opening_line)
+
+    chat_id = update.message.chat_id
+
+    movie_name = update.message.text[7:].strip()
+
+    context.bot.send_message(chat_id=chat_id, text="No movie available for this title: " + movie_name)
+
+async def help_command(update, context):
     await update.message.reply_text("Help!")
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
+async def echo(update, context):
     await update.message.reply_text(update.message.text)
 
-def main() -> None:
-    application = Application.builder().token("6115361738:AAEM_3vaL1cktBuSSClReD0-CIWDHtopcU4").build()
 
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+def main():
+    updater = Updater(BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
 
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    dispatcher.add_handler(CommandHandler("movie", movie_handler))
 
-    application.run_polling()
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    dispatcher.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == "__main__":

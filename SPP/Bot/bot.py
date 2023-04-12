37efd823c009cd7ebb524161e37f93c6306771f7
VilardCool <61537368+VilardCool@users.noreply.github.com>
2023-04-12 11:17:26 +0300

    Update bot.py

--- SPP/Bot/bot.py
+++ SPP/Bot/bot.py
@@ -27,12 +27,68 @@ async def get_channel_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -
                                         '\nViews: ' + r['items'][0]['statistics']['viewCount'] + 
                                         '\nSubscribers: ' + r['items'][0]['statistics']['subscriberCount'] + 
                                         '\nVideos: ' + r['items'][0]['statistics']['videoCount'])
+        
+async def get_most_liked_video(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    channel = update.message.text[18:]
+    channelStat = get_service().channels().list(forUsername=channel, part='snippet,contentDetails,statistics').execute()
+
+    if channelStat["pageInfo"]["totalResults"] == 0:
+        await update.message.reply_text('Channel not found')
+        return
+    
+    allUploadedVideosPlaylist =  channelStat["items"][0]['contentDetails']['relatedPlaylists']['uploads']
+    videos = [ ]
+    next_page_token = None
+
+    while True:
+        playlistData = get_service().playlistItems().list(playlistId=allUploadedVideosPlaylist, part='snippet', maxResults=50, pageToken=next_page_token).execute()
+        videos += playlistData['items']
+        next_page_token = playlistData.get('nextPageToken')
+
+        if next_page_token is None:
+            break
+    
+    video_ids=[]    
+    for i in range(len(videos)):
+        video_ids.append(videos[i]["snippet"]["resourceId"]["videoId"])
+        i+=1
+
+    videoStatistics = []
+    for i in range(len(video_ids)):
+        videoData = get_service().videos().list(id=video_ids[i],part = "statistics").execute()
+        videoStatistics.append(videoData["items"][0]["statistics"])
+        i+=1
+
+    VideoTitle=[ ]
+    url=[ ]
+    Published = [ ]
+    Views=[ ]
+    LikeCount=[ ]
+    ml = 0
+    mli = 0
+
+    for i in range(len(videos)):
+        VideoTitle.append((videos[i])['snippet']['title'])
+        url.append("https://www.youtube.com/watch?v="+(videos[i])['snippet']['resourceId']['videoId'])
+        Published.append((videos[i])['snippet']['publishedAt'])
+        Views.append(int((videoStatistics[i])['viewCount']))
+        LikeCount.append(int((videoStatistics[i])['likeCount']))
+        if (int((videoStatistics[i])['likeCount']) > ml):
+            ml = int((videoStatistics[i])['likeCount'])
+            mli = i
+
+    await update.message.reply_text('Most liked video: ' + VideoTitle[mli] +
+                                    '\nWith ' + str(ml) + ' likes' +
+                                    '\nAnd ' + str(Views[i]) + ' views' +
+                                    '\nWas published: ' + Published[i] +
+                                    '\n' + url[i])
 
 def main() -> None:
     application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("channel", get_channel_info))
+    application.add_handler(CommandHandler("most_liked_video", get_most_liked_video))
 
     application.run_polling()
 
